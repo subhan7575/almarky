beff5cf6a8046424c61b02c78ebe0140deb30876
subhan7575 <saifujafar895@gmail.com>
2026-02-01 18:07:43 +0500

    Update Model.tsx

--- components/ui/Model.tsx
+++ components/ui/Model.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import useClickOutside from '../../hooks/useClickOutside';
+import useClickOutside from '../hooks/useClickOutside';
 
 interface ModalProps {
   isOpen: boolean;
