fdd7de4f346c86d63fbea5ebec6b28803039fcd5
subhan7575 <saifujafar895@gmail.com>
2026-02-02 16:29:06 +0500

    Update QuickSelect.tsx

--- components/QuickSelect.tsx
+++ components/QuickSelect.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { Product } from '../types';
 import { useCart } from '../context/CartContext';
 import { useAuth } from '../context/AuthContext';
-import Modal from './ui/Modal.tsx';
+import Modal from './ui/Modal';
 
 interface QuickSelectProps {
   product: Product;
