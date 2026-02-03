260452305fb591ce70c874402a6c0a94a1419579
subhan7575 <saifujafar895@gmail.com>
2026-02-03 17:07:22 +0500

    Update Checkout.tsx

--- pages/Checkout.tsx
+++ pages/Checkout.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
 import { useOrders } from '../context/OrderContext';
 import { useAuth } from '../context/AuthContext';
-import { getGoogleScriptUrl } from '../utils/security';
+
 
 const Checkout: React.FC = () => {
   const { cart, totalPayable, clearCart } = useCart();
